126f519cd8a6d211e3a55f43d5546a32a67dc439
yacinoc <137206550+yacinoxo@users.noreply.github.com>
2025-01-14 12:15:02 +0000

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,14 @@
 "use client"
 import { Jaro } from "next/font/google";
-import { useRef } from "react";
-import { FaCss3Alt, FaHtml5 } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-import {motion, useInView } from "motion/react";
-import { FaReact } from "react-icons/fa";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip"
-import { DiJavascript } from "react-icons/di";
-import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
-import { GrMysql } from "react-icons/gr";
-import { IoLogoFirebase } from "react-icons/io5";
-import { SiExpress, SiFlask } from "react-icons/si";
-import { TbBrandFramerMotion, TbBrandNodejs } from "react-icons/tb";
+import {motion } from "motion/react";
+
 import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 
 const jaro = Jaro({ subsets : ['latin']})
 
 export default function Home() {
-
-  const skillsRef = useRef<HTMLParagraphElement>(null)
-  const isInViewSkill = useInView(skillsRef, {once: true})
-
-
-
-
 
   return (
     <div className="bg-[#2A004E] !w-screen min-h-screen !h-fit p-10 max-sm:p-4 pb-0 overflow-y-auto !overflow-x-hidden scroll-smooth !no-scrollbar">
@@ -46,135 +26,7 @@ export default function Home() {
       <div className="flex justify-center mb-20">
       <main className="flex w-full relative max-sm:p-4 !max-w-6xl h-fit py-8 flex-col !justify-between !items-center gap-4 main">
           <Projects/>
-          <motion.div initial={{top:100, opacity:0}} animate={isInViewSkill ? {opacity: 100 ,top: 0,transition: {duration: 1.5}} : {}} id="projects" className="w-fit max-sm:max-w-6xl !scale-[.65] max-w-2xl overflow-hidden relative rounded bg-[#2E2E50] !p-6">
-            <p className={`${jaro.className} text-white text-7xl mb-8`} ref={skillsRef}>Skills</p>
-            <div className="w-[470px] h-fit flex !justify-between !col-span-2 mb-8">
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <FaHtml5 size={80} color="red"/>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Html</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <FaCss3Alt size={80} color="cyan"/>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Css</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <DiJavascript size={80} color="yellow"/>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>JavaScript</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <FaReact size={80} color="cyan"/>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>React</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            </div>
-            <div className="w-[470px] h-fit flex !justify-between !col-span-2 mb-8">
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <RiTailwindCssFill size={80} color="aqua"/>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Tailwind</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <RiNextjsFill size={80} color="white"/>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Next Js</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <GrMysql size={80} color="aqua"/>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>MySql</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <IoLogoFirebase size={80} color="orange"/>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>FireBase</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            </div>
-            <div className="w-[470px] h-fit flex !justify-between !col-span-2">
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <SiExpress size={80} color="white"/>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Express</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <TbBrandFramerMotion size={80} color="purple"/>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Framer Motion</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <SiFlask size={80} color="white"/>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Flask</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <TbBrandNodejs size={80} color="Chartreuse"/>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Node</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            </div>
-          </motion.div>
+          <Skills/>
         </main>
       </div>
       <div className="flex justify-center">
