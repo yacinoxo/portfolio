02199ff13762b21c0ecf39476147fb9b4cabbbcd
yacinoc <137206550+yacinoxo@users.noreply.github.com>
2025-01-15 21:31:58 +0000

    push

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 'use client';
 
-import { Lalezar } from 'next/font/google';
+import { Jaro, Lalezar } from 'next/font/google';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 
+const jaro = Jaro({
+  subsets: ["latin"]
+})
 const lalezar = Lalezar({
   subsets: ['arabic'],
   weight: "400"
@@ -28,10 +31,10 @@ export default function Header() {
     <header className="fixed top-0 left-0 right-0 bg-[#1E1E3F]/80 backdrop-blur-md z-50" dir={currentLang === "en" ? "ltr" : "rtl"}>
       <nav className="max-w-4xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
-          <h1 className="text-3xl font-bold text-[#FF00FF] !font-[jaro] ">Yacinoxo</h1>
+          <h1 className={`text-3xl font-bold text-[#FF00FF] ${jaro.className}`}>Yacinoxo</h1>
           <Link href={currentLang === "en" ? "/ar" : "/"}
             onClick={toggleLanguage}
-            className={`${currentLang === 'en' ? lalezar.className : "!font-[Jaro]"} px-4 py-2 rounded bg-[#2D2B55] text-white hover:bg-[#373561] transition-colors`}
+            className={`${currentLang === 'en' ? lalezar.className : jaro.className} px-4 py-2 rounded bg-[#2D2B55] text-white hover:bg-[#373561] transition-colors`}
           >
             {currentLang === 'en' ? 'العربية' : 'English'}
           </Link>
