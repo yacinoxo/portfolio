71cd793b228fc5d1a32e9f588e50b4d702430679
yacinoc <137206550+yacinoxo@users.noreply.github.com>
2025-01-15 12:48:19 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics/>
         {children}
       </body>
     </html>
