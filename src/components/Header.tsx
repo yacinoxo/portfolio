6b51a5ef8558bd65c5dd459f4465b971aa5403ba
yacinoc <137206550+yacinoxo@users.noreply.github.com>
2025-01-15 21:28:52 +0000

    push

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,13 +25,13 @@ export default function Header() {
 
 
   return (
-    <header className="fixed top-0 left-0 !max-w-6xl right-0 bg-[#1E1E3F]/80 backdrop-blur-md z-50" dir={currentLang === "en" ? "ltr" : "rtl"}>
+    <header className="fixed top-0 left-0 right-0 bg-[#1E1E3F]/80 backdrop-blur-md z-50" dir={currentLang === "en" ? "ltr" : "rtl"}>
       <nav className="max-w-4xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
-          <h1 className="text-3xl font-bold text-[#FF00FF] font-[jaro] ">Yacinoxo</h1>
+          <h1 className="text-3xl font-bold text-[#FF00FF] !font-[jaro] ">Yacinoxo</h1>
           <Link href={currentLang === "en" ? "/ar" : "/"}
             onClick={toggleLanguage}
-            className={`${currentLang === 'en' ? lalezar.className : "font-[Jaro]"} px-4 py-2 rounded bg-[#2D2B55] text-white hover:bg-[#373561] transition-colors`}
+            className={`${currentLang === 'en' ? lalezar.className : "!font-[Jaro]"} px-4 py-2 rounded bg-[#2D2B55] text-white hover:bg-[#373561] transition-colors`}
           >
             {currentLang === 'en' ? 'العربية' : 'English'}
           </Link>
