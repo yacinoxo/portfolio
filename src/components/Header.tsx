711eb800945085f70467bd00c5d27c3e15022461
yacinoc <137206550+yacinoxo@users.noreply.github.com>
2025-01-15 21:27:21 +0000

    push

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
 
 
   return (
-    <header className="fixed top-0 left-0 right-0 bg-[#1E1E3F]/80 backdrop-blur-md z-50" dir={currentLang === "en" ? "ltr" : "rtl"}>
+    <header className="fixed top-0 left-0 !max-w-6xl right-0 bg-[#1E1E3F]/80 backdrop-blur-md z-50" dir={currentLang === "en" ? "ltr" : "rtl"}>
       <nav className="max-w-4xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <h1 className="text-3xl font-bold text-[#FF00FF] font-[jaro] ">Yacinoxo</h1>
