2efd83a8e22d84699f0dc45d8fd582ee22a3bef4
yacinoc <137206550+yacinoxo@users.noreply.github.com>
2025-01-15 11:58:42 +0000

    push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,11 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Analytics/>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
         {children}
+        <Analytics/>
       </body>
     </html>
   );
