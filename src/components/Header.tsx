d93473c41f60b2f8851fb2a9855518f7c588e855
yacinoc <137206550+yacinoxo@users.noreply.github.com>
2025-01-15 21:26:17 +0000

    push

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
 'use client';
 
+import { Lalezar } from 'next/font/google';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
+
+const lalezar = Lalezar({
+  subsets: ['arabic'],
+  weight: "400"
+})
 
 
 
@@ -25,7 +31,7 @@ export default function Header() {
           <h1 className="text-3xl font-bold text-[#FF00FF] font-[jaro] ">Yacinoxo</h1>
           <Link href={currentLang === "en" ? "/ar" : "/"}
             onClick={toggleLanguage}
-            className={`${currentLang === 'en' ? "font-[lalezar]" : "font-[jaro]"} px-4 py-2 rounded bg-[#2D2B55] text-white hover:bg-[#373561] transition-colors`}
+            className={`${currentLang === 'en' ? lalezar.className : "font-[Jaro]"} px-4 py-2 rounded bg-[#2D2B55] text-white hover:bg-[#373561] transition-colors`}
           >
             {currentLang === 'en' ? 'العربية' : 'English'}
           </Link>
